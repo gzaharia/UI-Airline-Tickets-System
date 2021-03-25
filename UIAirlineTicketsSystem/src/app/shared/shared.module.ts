ed35aa5d3d09be24f47064ea364185ff42b89b2c
MihaiGaidau <gaidaumihai@gmail.com>
2021-03-25 22:24:20 +0200

    ATS-15-Add-Calendar-modal

--- UIAirlineTicketsSystem/src/app/shared/shared.module.ts
+++ UIAirlineTicketsSystem/src/app/shared/shared.module.ts
@@ -1,4 +1,4 @@
-import {NgModule} from '@angular/core';
+import {NgModule, NO_ERRORS_SCHEMA} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {FooterComponent} from './components/footer/footer.component';
 import {HeaderComponent} from './components/header/header.component';
@@ -6,20 +6,30 @@ import {TransformPipe} from './pipes/transform.pipe';
 import {RouterModule} from '@angular/router';
 import {TranslateModule} from '@ngx-translate/core';
 import { SearchFlightComponent } from './components/search-flight/search-flight.component';
+import { BasicCalendarComponent } from './components/basic-calendar/basic-calendar.component';
+import {CalendarModule} from 'primeng/calendar';
+import { SelectDateModalComponent } from './components/select-date-modal/select-date-modal.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 
 @NgModule({
-  declarations: [FooterComponent, HeaderComponent, TransformPipe, SearchFlightComponent],
+  declarations: [FooterComponent, HeaderComponent, TransformPipe, SearchFlightComponent, BasicCalendarComponent, SelectDateModalComponent],
   imports: [
     CommonModule,
     RouterModule,
-    TranslateModule
+    TranslateModule, 
+    CalendarModule,
+    ReactiveFormsModule,
+    FormsModule
   ],
   exports: [
     FooterComponent,
     HeaderComponent,
     TransformPipe,
     SearchFlightComponent
+  ],
+  schemas: [
+    NO_ERRORS_SCHEMA
   ]
 })
 export class SharedModule {
