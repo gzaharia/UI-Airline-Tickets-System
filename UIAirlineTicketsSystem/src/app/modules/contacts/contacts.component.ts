5174c2ca9e4d96a5d16c3e5d26d532064062a91e
MihaiGaidau <gaidaumihai@gmail.com>
2021-04-17 10:56:01 +0300

    ATS-26-Create-services-page

--- UIAirlineTicketsSystem/src/app/modules/contacts/contacts.component.ts
+++ UIAirlineTicketsSystem/src/app/modules/contacts/contacts.component.ts
@@ -7,9 +7,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ContactsComponent implements OnInit {
 
+  public contacts: number[] = [1, 1];
+  public paragraphs: number[][] = [];
+  public list: string[];
+
   constructor() { }
 
+  /** add terms array */
+  private initData(): void {
+    for (const d of this.contacts){
+      this.paragraphs.push(Array(d).fill(1));
+    }
+  }
+
+
   ngOnInit(): void {
+    this.initData();
   }
 
 }
--- UIAirlineTicketsSystem/src/app/modules/services/services.component.ts
+++ UIAirlineTicketsSystem/src/app/modules/services/services.component.ts
@@ -7,9 +7,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ServicesComponent implements OnInit {
 
+  public services: number[] = [1, 1, 1];
+  public paragraphs: number[][] = [];
+  public list: string[];
+
   constructor() { }
 
+  /** add terms array */
+  private initData(): void {
+    for (const d of this.services){
+      this.paragraphs.push(Array(d).fill(1));
+    }
+
+    // for (let i = 0; i < this.abouts.length; i++) {
+    //   this.paragraphs.push(Array(this.abouts[i]).fill(1));
+    // }
+  }
+
+
   ngOnInit(): void {
+    this.initData();
   }
 
 }
